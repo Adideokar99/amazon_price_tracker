01a751546deaf8a00460fd3729181ce8e4001688
Aditya Deokar <aditya.sdeokar@gmail.com>
2021-03-04 20:35:55 -0500

    Fixed Scraping

--- scraper.py
+++ scraper.py
@@ -1,31 +1,98 @@
-import requests
 from bs4 import BeautifulSoup
-
-def extract_url(url):
-
-    if url.find("www.amazon.com") != -1:
-        index = url.find("/dp/")
-        if index != -1:
-            index2 = index + 14
-            url = "https://www.amazon.com" + url[index:index2]
-        else:
-            index = url.find("/gp/")
-            if index != -1:
-                index2 = index + 22
-                url = "https://www.amazon.com" + url[index:index2]
-            else:
-                url = None
-    else:
-        url = None
-    return url
-
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0"}
-
-def get_converted_price(price):
-    stripped_price = price.strip("$ ,")
-    replaced_price = stripped_price.replace(",", "")
-    converted_price = float(replaced_price)
-
-    return converted_price
-
-print(get_converted_price("$50.00"))
+import requests
+ 
+# Function to extract Product Title
+def get_title(soup):
+     
+    try:
+        # Outer Tag Object
+        title = soup.find("span", attrs={"id":'productTitle'})
+ 
+        # Inner NavigableString Object
+        title_value = title.string
+ 
+        # Title as a string value
+        title_string = title_value.strip()
+ 
+        # # Printing types of values for efficient understanding
+        # print(type(title))
+        # print(type(title_value))
+        # print(type(title_string))
+        # print()
+ 
+    except AttributeError:
+        title_string = ""   
+ 
+    return title_string
+ 
+# Function to extract Product Price
+def get_price(soup):
+ 
+    try:
+        price = soup.find("span", attrs={'id':'priceblock_ourprice'}).string.strip()
+ 
+    except AttributeError:
+        price = ""  
+ 
+    return price
+ 
+# Function to extract Product Rating
+def get_rating(soup):
+ 
+    try:
+        rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
+         
+    except AttributeError:
+         
+        try:
+            rating = soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
+        except:
+            rating = "" 
+ 
+    return rating
+ 
+# Function to extract Number of User Reviews
+def get_review_count(soup):
+    try:
+        review_count = soup.find("span", attrs={'id':'acrCustomerReviewText'}).string.strip()
+         
+    except AttributeError:
+        review_count = ""   
+ 
+    return review_count
+ 
+# Function to extract Availability Status
+def get_availability(soup):
+    try:
+        available = soup.find("div", attrs={'id':'availability'})
+        available = available.find("span").string.strip()
+ 
+    except AttributeError:
+        available = ""  
+ 
+    return available    
+ 
+if __name__ == '__main__':
+ 
+    # Headers for request
+    HEADERS = ({'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81',
+                'Accept-Language': 'en-US, en;q=0.5'})
+ 
+    # The webpage URL
+    URL = "https://www.amazon.com/fire-tv-stick-with-alexa-voice-remote/dp/B07ZZVX1F2/ref=sr_1_1?dchild=1&keywords=amazon+fire+stick&qid=1614904098&sr=8-1"
+ 
+    # HTTP Request
+    webpage = requests.get(URL, headers=HEADERS)
+ 
+    # Soup Object containing all data
+    soup = BeautifulSoup(webpage.content, "lxml")
+ 
+    # Function calls to display all necessary product information
+    print("Product Title =", get_title(soup))
+    print("Product Price =", get_price(soup))
+    print("Product Rating =", get_rating(soup))
+    print("Number of Product Reviews =", get_review_count(soup))
+    print("Availability =", get_availability(soup))
+    print()
+    print()
